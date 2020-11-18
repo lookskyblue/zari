0b67fc5b733d9e6fd464596f119d22cf2c2223cc
unknown <lookskyblue111@gmail.com>
2020-11-18 14:24:06 +0900

    login1.0.1

--- LOGIN/login.js
+++ LOGIN/login.js
@@ -1,21 +1,24 @@
+/*
+    HTML form 에서 로그인 버튼 클릭 시 입력한 id값을 콘솔에 출력함.
+    실제 구현은 입력 값들을 Firebase로 넘겨줄 것. 
+*/
+
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input");
 const USER_VALUE = "D A T A"
 
 function saveValue(text) {
-    console.log("hi");
     localStorage.setItem(USER_VALUE, text);
+    console.log(text + " is storaged successfully")
 }
 
 function handleSubmit(event) {
-    console.log("hi");
     event.preventDefault();
     const inputValue = input.value;
     saveValue(inputValue);
 }
 
 function init() {
-    console.log("hi");
     form.addEventListener("submit", handleSubmit); 
 }
 
