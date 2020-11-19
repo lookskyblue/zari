0a11d7d96ed470bbd8e6f4d16e36d1ba14a7e782
unknown <kim70273@naver.com>
2020-11-20 01:41:08 +0900

    First Day

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function App() {
-  return <div>우성 바보</div>;
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Router } from "react-router-dom";
+import AppRouter from "./Router";
+
+function App() {
+  return <AppRouter/>;
+}
+
+export default App;
