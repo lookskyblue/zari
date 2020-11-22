ced006ca6080f2b0657181d7359ab260af593bb5
unknown <lookskyblue111@gmail.com>
2020-11-23 01:07:04 +0900

    Simple ShowStoreList

--- src/routes/ShowStoreList.js
+++ src/routes/ShowStoreList.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "fbase";
 
 const ShowStoreList = () => {
+    const [storeList, setStoreList] = useState([]);
+
+    const getStoreList = async () => { // 매장 컬렉션 가져오기
+        const dbStoreInfo = await dbService.collection("storeinfo").get();
+        dbStoreInfo.forEach((document) => {
+            const storeListObject = { // 객체임
+                ...document.data(),
+                id: document.id,
+            };
+            setStoreList((prev) => [storeListObject, ...prev]);
+        });
+    };
+
+    useEffect(() => { //컴포넌트가 마운트 되면 매장 정보를 가져 오겠다 2ㄱ
+        getStoreList();
+    }, []);
+
+    //console.log(storeList);
     return(
-        <h5>힝 아무것도 없찌롱</h5>
+        <div>
+            {storeList.map((obj) => (
+                <div key={obj.id}>
+                    <h4>
+                        가게이름: {obj.storeName}  
+                    </h4> 
+                </div>
+            ))}
+        </div>
     );
-}
+};
 
 export default ShowStoreList;
