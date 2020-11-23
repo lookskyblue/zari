bba508a5026781ead05b01e3633235a3e8d1746f
unknown <sekn1997@naver.com>
2020-11-24 01:29:08 +0900

    css 수정

--- src/components/StoreInfo.js
+++ src/components/StoreInfo.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import GoogleApiWrapper from "./googlemap";
+import Profile from "../routes/Profile";
+import {Link} from "react-router-dom";
 
-const StoreInfo = ({storeObj,isOwner})=>(
-    <div >
-                <h3>
-                    매장이름: {storeObj.storeName}
-                </h3>
-                <h4>매장정보: {storeObj.storeIntro}</h4>
-                
-                {isOwner && (
-                    <>
-                    <button>선택</button>
-                    </>
-                )}
+const StoreInfo = ({ storeObj, isOwner }) => (
+    <div className="storeInfo">
+        <Link to="/Home">
+        <h3 className="storeInfo__name">
+            매장이름: {storeObj.storeName}
+        </h3>
+        <h4 className="storeInfo__intro">
+            매장정보: {storeObj.storeIntro}
+        </h4>
+
+        {isOwner && (
+            <>
+                <button>선택</button>
+            </>
+        )}
+        </Link>
     </div>
-    
-        
-    
 )
 
 export default StoreInfo;
--- src/routes/ShowStoreList.js
+++ src/routes/ShowStoreList.js
@@ -5,28 +5,28 @@ import StoreInfo from "components/StoreInfo";
 import userEvent from "@testing-library/user-event";
 import SimpleMap from "../components/googlemap"
 
-const ShowStoreList = ({userObj}) => {
+const ShowStoreList = ({ userObj }) => {
     const [storeList, setStoreList] = useState([]);
 
     useEffect(() => { //컴포넌트가 마운트 되면 매장 정보를 가져 오겠다 2ㄱ
-        
+
         dbService.collection("storeinfo").onSnapshot(snapshot => {
             const storeArray = snapshot.docs.map(doc => ({
-                id:doc.id, 
+                id: doc.id,
                 ...doc.data(),
             }));
             setStoreList(storeArray);
         });
     }, []);
-    
-    
-    
-    return(
-        
+
+
+
+    return (
+
         <div>
             <SimpleMap />
             {storeList.map((obj) => (
-                <StoreInfo key={obj.id} storeObj={obj} isOwner={obj.storeOnwer === userObj.email}/>
+                <StoreInfo key={obj.id} storeObj={obj} isOwner={obj.storeOnwer === userObj.email} />
             ))}
         </div>
     );//여기서 css수정
