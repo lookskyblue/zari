e795dc014c7eb7271a4a4fb81b348aa55fc99b41
unknown <kim70273@naver.com>
2020-11-25 23:45:29 +0900

    매장등록 위치에러 수정및 테이블수 에러수정.

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,6 @@ function App() {
   if (navigator.geolocation) { // GPS를 지원하면
     navigator.geolocation.getCurrentPosition(pos=>{
         setLocation(pos.coords);
-        
     },
     error =>{
         console.error(error);
@@ -23,11 +22,21 @@ function App() {
         maximumAge: 0,
         timeout: Infinity
     }
-    ); 
+    );
   } else {
     alert('위치정보 불러오기 실패');
   }
 
+  
+  console.log(location);
+  localStorage.setItem(
+    "userLocation",
+    JSON.stringify({
+      latitude: location,
+      longitude: location
+    })
+  );
+  console.log(localStorage.getItem("userLocation"));
   
   useEffect(() => {
     authService.onAuthStateChanged((user)=> {
@@ -44,6 +53,7 @@ function App() {
   return (
     <>
       {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} location={location}/> : "Initializing..."}
+      {console.log(location)}
       <footer>&copy; {new Date().getFullYear()} ZARI</footer>
     </>
   );
