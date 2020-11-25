5b559ddfad6d467a7c203176e00ff959578288b1
unknown <kim70273@naver.com>
2020-11-25 18:37:01 +0900

    새로고침시 기존 정보유지.

--- src/routes/PosEdit.js
+++ src/routes/PosEdit.js
@@ -7,22 +7,26 @@ class PosEdit extends React.Component {
 
 
     componentDidMount() {
-       // const { location, history } = this.props;
-        //console.log(location.state.storeObj);
-        //if(storeObj === undefined) {
-            //isOwner.push("/");            
-       // }
+        const { location, history } = this.props;
+        //로컬 스토리지에 저장해서 새로고침해도 상관없도록!
+       if(location.state !== undefined){ 
+        localStorage.setItem(
+            "userInfo",
+            JSON.stringify({
+              location: this.props.location,
+              history: this.props.history
+            })
+          );
+          console.log(JSON.parse(localStorage.getItem("userInfo")));
+        }
     }
 
     render() {
-        const { location, history } = this.props;
-        
-        
-       // const { location, history } = this.props;
-        // console.log(this.props);
-        // console.log(location.state);
-         //console.log(location.state.storeObj);
-        //<ReviewPage storeName={storeName} ownerId={ownerId}/>
+        if(localStorage.getItem("userInfo")){
+            this.props= JSON.parse(localStorage.getItem("userInfo"));
+            }//새로고침시 로컬호스트에 저장된 정보가 있다면 받아온다.
+            const { location, history } = this.props; 
+            
         return (
         <div> 
             <ul>
